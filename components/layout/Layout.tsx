65f465dbb7f2e1d6fde9abd2bc286bf608eda4f8
farhad <rb0214192@gmail.com>
2023-06-16 21:56:53 +0330

    created layout using mantine

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,25 +1,31 @@
+import { Box, Center, Container, Group, Text } from "@mantine/core";
 import Link from "next/link";
 import { ReactNode } from "react";
 
-function Layout({
-  children,
-}: {
-  children: ReactNode;
-}) {
+function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <header className="header">
-        <h2> Customer Management System</h2>
-        <Link href="/add-customer">Add Customer</Link>
-      </header>
-      <div className="main">{children}</div>
-      <footer className="footer">
-        <a href="https://botostart.ir" target="_blank" rel="noreferrer">
-          Botostart
-        </a>{" "}
-        Next.js course | CRM Project &copy;
-      </footer>
-    </>
+    <Container size={"lg"}>
+      <Group position="apart" my={25}>
+        <Text color="white" fz={"xl"} component="h1">
+          {" "}
+          Customer Management System
+        </Text>
+        <Link
+          href="/add-customer"
+          className="bg-green-500 p-3 rounded-lg font-bold hover:bg-green-600 hover:transition-colors hover:duration-500 duration-500"
+        >
+          Add Customer
+        </Link>
+      </Group>
+      <Box mih={"75vh"}>{children}</Box>
+      <Box className="bg-[#414459] p-5 rounded-lg my-4">
+        <Center>
+          <Text color="white" fz={"sm"}>
+            Next.js | Customer Management System &copy;
+          </Text>
+        </Center>
+      </Box>
+    </Container>
   );
 }
 
