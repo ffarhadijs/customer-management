f6fe4747762dcfb993299a8222f193cafd535353
farhad <rb0214192@gmail.com>
2023-06-19 00:24:34 +0330

    add mantine provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
+import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <MantineProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </MantineProvider>
   );
 }
