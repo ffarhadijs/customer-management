93814b05f8151b09bcb396bde8e13d8db48c4e9c
farhad <rb0214192@gmail.com>
2023-06-21 13:07:52 +0330

    notification provider added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
 import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider>
+      <Notifications/>
       <Layout>
         <Component {...pageProps} />
       </Layout>
