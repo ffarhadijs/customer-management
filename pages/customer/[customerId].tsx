92accda30bdd2aa8e7881a753059cc97b4e8d736
farhad <rb0214192@gmail.com>
2023-06-21 13:05:28 +0330

    customer detail page updated

--- pages/customer/[customerId].tsx
+++ pages/customer/[customerId].tsx
@@ -1,11 +1,15 @@
-import { Box } from "@mantine/core";
+import { Box, Flex, Grid, Text } from "@mantine/core";
 import { useRouter } from "next/router";
 import React from "react";
 import { useEffect, useState } from "react";
 import { UserType } from "@/types/user.type";
+
+type User = {
+  data: UserType;
+};
 const CustomerDetails = () => {
   const { query, isReady } = useRouter();
-  const [customer, setCustomer] = useState<UserType | null>();
+  const [customer, setCustomer] = useState<User | null>();
 
   const fetchData = async () => {
     const response = await fetch(`/api/customer/${query.customerId}`);
@@ -19,7 +23,75 @@ const CustomerDetails = () => {
     }
   }, [isReady]);
   console.log(customer, "customer");
-  return <Box></Box>;
+  return (
+    <Box>
+      {customer?.data._id ? (
+        <Grid className="bg-[#414459] p-5 my-5 rounded-lg text-white">
+          <Grid.Col xs={12} sm={6}>
+            <Text fw={600}>
+              Name:{"  "}
+              <Text fw={400} component="span">
+                {customer?.data.name}
+              </Text>{" "}
+            </Text>
+          </Grid.Col>
+          <Grid.Col xs={12} sm={6}>
+            <Text fw={600}>
+              Last Name:{"  "}
+              <Text fw={400} component="span">
+                {customer?.data.lastName}
+              </Text>{" "}
+            </Text>
+          </Grid.Col>{" "}
+          <Grid.Col xs={12} sm={6}>
+            <Text fw={600}>
+              Email:{"  "}
+              <Text fw={400} component="span">
+                {customer?.data.email}
+              </Text>{" "}
+            </Text>
+          </Grid.Col>{" "}
+          <Grid.Col xs={12} sm={6}>
+            <Text fw={600}>
+              Address:{"  "}
+              <Text fw={400} component="span">
+                {customer?.data.address}
+              </Text>{" "}
+            </Text>
+          </Grid.Col>{" "}
+          <Grid.Col xs={12} sm={6}>
+            <Text fw={600}>
+              Postal Code:{"  "}{" "}
+              <Text fw={400} component="span">
+                {" "}
+                {customer?.data.postalCode}{" "}
+              </Text>
+            </Text>
+          </Grid.Col>{" "}
+          <Grid.Col span={12}>
+            <Text>
+              Products:{" "}
+              {customer?.data.products?.map((item) => (
+                <Grid my="md" ml={"80px"} className="bg-[#5e627e] rounded-lg p-2">
+                  <Grid.Col xs={6} sm={4}>
+                    <Text>Product Name: {item.productName}</Text>
+                  </Grid.Col>
+                  <Grid.Col xs={3} sm={4}>
+                    <Text>Price: $ {item.price}</Text>
+                  </Grid.Col>
+                  <Grid.Col xs={3} sm={4}>
+                    <Text>Quantity: {item.quantity}</Text>
+                  </Grid.Col>
+                </Grid>
+              ))}
+            </Text>{" "}
+          </Grid.Col>
+        </Grid>
+      ) : (
+        <Text c={"green"}>loading...</Text>
+      )}
+    </Box>
+  );
 };
 
 export default CustomerDetails;
